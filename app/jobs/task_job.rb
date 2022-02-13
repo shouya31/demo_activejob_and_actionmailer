72a88ca1a4d371818d465f8c395680cb926d7b8d
shouya31 <shouya0331@ezweb.ne.jp>
2022-02-13 18:55:48 +0900

    Add after_perform

--- app/jobs/task_job.rb
+++ app/jobs/task_job.rb
@@ -1,7 +1,18 @@
 class TaskJob < ApplicationJob
   queue_as :default
 
-  def perform(title="掃除")
+  after_perform do |job|
+    user = job.arguments.last
+    send_mail(user)
+  end
+
+  def perform(title, user)
     Task.create!(title: title)
+  end
+
+  private
+
+  def send_mail(user)
+    UserMailer.with(user: user).welcome_email.deliver_later
   end
 end
